a8b4882e78bcfa4d26a5db7bacc0e725bbae1615
Zoltan Nyitrai <58224785+zoltannyitrai@users.noreply.github.com>
2025-06-25 10:46:06 +0200

    Update streamlit_app.py
    new api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
